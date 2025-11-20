dcc04ea925eaa27bd9ae564e03fce775f7d4a3bc
Prabhadee Paranavithana <pprabhadee@gmail.com>
2025-11-20 14:15:41 +0530

    add fn link

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" , "https://smart-blog-frontend-ten.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
